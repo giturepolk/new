f9f072364591d04df5d238087ddf78a5c110a69a
giturepolk <108657394+giturepolk@users.noreply.github.com>
2022-07-07 08:10:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,27 @@
 #! /bin/sh
 
 ################ Let ################
-INSTALL="YES"
 WINDOWS_SHARE_DIR=from_ubuntu
 UBUNTU_SHARE_DIR=~/Desktop/to_windows
 DOWNLOAD_DIR=~/Downloads
 GIT_SRC=https://raw.githubusercontent.com/giturepolk/new/main/
+
 TOR_FILE=tor-browser-linux64-11.0.14_en-US.tar.xz
 TOR_EXTRACT_DIR="$DOWNLOAD_DIR"/tor-browser_en-US
 TOR_SHORTCUT=~/Desktop/Tor.sh
+
 ELECTRUM_FILE=Electrum-4.2.2.tar.gz
+ELECTRUM_EXTRACT_DIR="$DOWNLOAD_DIR"/Electrum-4.2.2
 ELECTRUM_SHORTCUT=~/Desktop/Electrum
 
 ################# update ##################
-if [ "$INSTALL" = "YES" ]; then
-	sudo apt update
-	sudo apt upgrade
-fi
-
-################# packages ################
-if [ "$INSTALL" = "YES" ]; then
-	sudo apt-get isntall tor --assume-yes
-	sudo apt-get install python3-pyqt5 --assume-yes
-	sudo apt-get install libsecp256k1-0 --assume-yes
-	sudo apt-get install python3-cryptography --assume-yes
-	sudo apt-get install python3-setuptools --assume-yes
-	sudo apt-get install python3-pip --assume-yes
-fi
+sudo apt update
+sudo apt-get isntall tor --assume-yes
+sudo apt-get install python3-pyqt5 --assume-yes
+sudo apt-get install libsecp256k1-0 --assume-yes
+sudo apt-get install python3-cryptography --assume-yes
+sudo apt-get install python3-setuptools --assume-yes
+sudo apt-get install python3-pip --assume-yes
 
 ################# Folders #################
 if [ ! -d "$UBUNTU_SHARE_DIR" ]; then
@@ -61,24 +56,26 @@ cd "$DOWNLOAD_DIR"
 if [ ! -f "$ELECTRUM_FILE" ]; then
 	echo "Download -> Electrum"
 	wget "$GIT_SRC"/"$ELECTRUM_FILE"
-	sudo pip3 install "$DOWNLOAD_DIR"/"$ELECTRUM_FILE"
+	tar -xvzf "$ELECTRUM_FILE"
 else
 	echo "Electrum already installed"
 fi
-electrum &
+cd "$ELECTRUM_EXTRACT_DIR"
+./run_electrum &
 
 ################ shortcuts #################
-if [ -f "$ELECTRUM_SHORTCUT" ]; then
-	sudo rm -rf "$ELECTRUM_SHORTCUT"
-fi
-ln -s /usr/local/bin/electrum "$ELECTRUM_SHORTCUT"
-
 if [ -f "$TOR_SHORTCUT" ]; then
 	sudo rm -rf "$TOR_SHORTCUT"
 fi
-
 echo "#! /bin/sh" >> "$TOR_SHORTCUT"
 echo cd "$TOR_EXTRACT_DIR" >> "$TOR_SHORTCUT"
 echo "./start-tor-browser.desktop" >> "$TOR_SHORTCUT"
 sudo chmod 777 "$TOR_SHORTCUT"
 
+if [ -f "$ELECTRUM_SHORTCUT" ]; then
+	sudo rm -rf "$ELECTRUM_SHORTCUT"
+fi
+echo "#! /bin/sh" >> "$ELECTRUM_SHORTCUT"
+echo cd "$ELECTRUM_EXTRACT_DIR" >> "$ELECTRUM_SHORTCUT"
+echo "./run_electrum" >> "$ELECTRUM_SHORTCUT"
+sudo chmod 777 "$ELECTRUM_SHORTCUT"
